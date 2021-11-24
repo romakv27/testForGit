b666b2fa708f3f9cface5264a86f5dc030b233ad
romakv27 <kvarchiyarom@gmail.com>
2021-11-24 09:08:02 +0300

    Add Cli

---
+++ src/main/java/ru/sfedu/library/utils/Cli.java
@@ -0,0 +1,17 @@
+package ru.sfedu.library.utils;
+
+import ru.sfedu.library.Constants;
+
+import java.io.IOException;
+
+import static ru.sfedu.library.Constants.SOUR;
+import static ru.sfedu.library.entity.enums.Outcomes.OK;
+import static ru.sfedu.library.utils.ConfigurationUtil.getConfigurationEntry;
+
+public class Cli {
+
+    public static void main(String []args) {
+        System.out.println(OK);
+
+    }
+}
