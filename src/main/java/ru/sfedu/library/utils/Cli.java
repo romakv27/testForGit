626c111ee7a287853d7e6280df482ee9709be555
romakv27 <kvarchiyarom@gmail.com>
2021-11-24 09:10:27 +0300

    Update Cli

--- src/main/java/ru/sfedu/library/utils/Cli.java
+++ src/main/java/ru/sfedu/library/utils/Cli.java
@@ -5,13 +5,14 @@ import ru.sfedu.library.Constants;
 import java.io.IOException;
 
 import static ru.sfedu.library.Constants.SOUR;
+import static ru.sfedu.library.entity.enums.Outcomes.ERROR;
 import static ru.sfedu.library.entity.enums.Outcomes.OK;
 import static ru.sfedu.library.utils.ConfigurationUtil.getConfigurationEntry;
 
 public class Cli {
 
     public static void main(String []args) {
-        System.out.println(OK);
+        System.out.println(ERROR);
 
     }
 }
